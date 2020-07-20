55b1ddb8ef04a4472809bf79e6132165e4186c94
Ngozi Kalu <kalungozi2@gmail.com>
2020-07-20 08:41:37 +0100

    ID card redesign initial change

--- Views/NewIdCardViewer.cs
+++ Views/NewIdCardViewer.cs
@@ -7,8 +7,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Drawing.Printing;
+using System.Drawing.Text;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -23,7 +25,7 @@ namespace LibraryManager.Views
         public NewIdCardViewer(Student student, bool action, string installedPrinter)
         {
             InitializeComponent();
-            getBarCode(student.qrcode);
+            getBarCode("*"+student.qrcode+"*");
             LoadReport(student, action, installedPrinter);
         }
         private string dirname = Guid.NewGuid().ToString();
@@ -33,26 +35,75 @@ namespace LibraryManager.Views
         int indexCurrentPage;
         bool bProcessingPages = true;
 
+        //void getBarCode(string data)
+        //{
+        //    BarcodeWriter wr = new BarcodeWriter();
+
+        //    wr.Format = BarcodeFormat.CODE_39;
+
+        //    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+        //    wr.Write(data).Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+        //    //create directory to save the image
+
+        //    if (!Directory.Exists(@"C:\" + dirname))
+        //    {
+        //        Directory.CreateDirectory(@"C:\" + dirname);
+        //    }
+
+        //    Bitmap bmp = new Bitmap(ms);
+        //    bmp.Save(@"C:\" + dirname + "\\" + dirname + ".jpg");
+
+        //    //return bmp;
+
+        //}
+
         void getBarCode(string data)
         {
-            BarcodeWriter wr = new BarcodeWriter();
+            string Barcode = data;
 
-            wr.Format = BarcodeFormat.CODE_93;
-
-            System.IO.MemoryStream ms = new System.IO.MemoryStream();
-
-            wr.Write(data).Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-            //create directory to save the image
-
-            if (!Directory.Exists(@"C:\" + dirname))
+            using (Bitmap bitmap = new Bitmap(350, 220))
             {
-                Directory.CreateDirectory(@"C:\" + dirname);
+                bitmap.SetResolution(240, 240);
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    Font font = new Font("IDAutomationHC39M", 10, FontStyle.Regular, GraphicsUnit.Point);
+
+                    graphics.Clear(Color.White);
+                    StringFormat stringformat = new StringFormat(StringFormatFlags.NoWrap);
+                    graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                    graphics.TextContrast = 10;
+                    SolidBrush black = new SolidBrush(Color.Black);
+                    SolidBrush white = new SolidBrush(Color.White);
+                    PointF TextPosition = new PointF(45F, 10F);
+                    SizeF TextSize = graphics.MeasureString(Barcode, font, TextPosition, stringformat);
+                    PointF pointPrice = new PointF(90f, 125f);
+                    Font newfont2 = new Font("Cambria", 8, FontStyle.Regular, GraphicsUnit.Point);
+                    Font newfont3 = new Font("Arial Black", 10, FontStyle.Regular, GraphicsUnit.Point);
+                    PointF pointPname = new PointF(200f, 170f);
+                    PointF pointBcode = new PointF(35f, 170f);
+                    graphics.DrawString("" + Barcode + "", newfont2, black, pointBcode);
+                    if (TextSize.Width > bitmap.Width)
+                    {
+                        float ScaleFactor = (bitmap.Width - (TextPosition.X / 2)) / TextSize.Width;
+                        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        graphics.ScaleTransform(ScaleFactor, ScaleFactor);
+                    }
+
+                    graphics.DrawString(Barcode, font, new SolidBrush(Color.Black), TextPosition, StringFormat.GenericTypographic);
+
+                    bitmap.Save(@"barcode.png", ImageFormat.Png);
+                    //this.pictureBox1.Image = (Bitmap)bitmap.Clone();
+                    if (!Directory.Exists(@"C:\" + dirname))
+                    {
+                        Directory.CreateDirectory(@"C:\" + dirname);
+                    }
+
+                    bitmap.Save(@"C:\" + dirname + "\\" + dirname + ".jpg", ImageFormat.Png);
+
+                    font.Dispose();
+                }
             }
-
-            Bitmap bmp = new Bitmap(ms);
-            bmp.Save(@"C:\" + dirname + "\\" + dirname + ".jpg");
-
-            //return bmp;
 
         }
         private void LoadReport(Student student, bool action, string InstalledIDCardPrinter)
